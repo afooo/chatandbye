8c74353351acefefc7c8163377f015994ed7d70e
Afoo Chang <foofoo.chang@gmail.com>
2016-02-05 23:38:51 +0800

    modify favicon and setting

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(session({ secret: 'chatchat' }));
 
 app.use('/', routes);
